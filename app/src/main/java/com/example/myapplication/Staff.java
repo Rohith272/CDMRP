f634402c9bd416f4bd4493ba458059cc72fa1834
Rohith272 <rohithk24280@gmail.com>
2020-06-29 21:58:16 +0530

    middle

--- app/src/main/java/com/example/myapplication/Staff.java
+++ app/src/main/java/com/example/myapplication/Staff.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 public class Staff extends AppCompatActivity {
     private Button button90;
     private Button button91;
+    private Button button92;
+    private Button button93;
+    private Button button94;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,39 @@ public class Staff extends AppCompatActivity {
                 opendmo();
             }
         });
+        button92 = findViewById(R.id.button92);
+        button92.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendp();
+            }
+        });
+        button93 = findViewById(R.id.button93);
+        button93.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opencclo();
+            }
+        });
+        button94 = findViewById(R.id.button94);
+        button94.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openot();
+            }
+        });
+    }
+    public void openot() {
+        Intent intent55 = new Intent(this,ot.class);
+        startActivity(intent55);
+    }
+    public void opencclo() {
+        Intent intent54 = new Intent(this,cclo.class);
+        startActivity(intent54);
+    }
+    public void opendp() {
+        Intent intent53 = new Intent(this,dp.class);
+        startActivity(intent53);
     }
     public void opendmo() {
         Intent intent52 = new Intent(this,dmo.class);
